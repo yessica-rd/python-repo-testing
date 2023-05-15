c410e4d8b0e56a1be5632c816399652fb70155a0
yessica-rd <yessica@leanmind.es>
2023-05-15 09:31:24 +0100

    Create sum and message methods

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-def method_1():
-    return "This is method 1"
+def sum(num1, num2):
+    return message(num1, num2)
 
 def method_2():
     return "This is method 2"
 
 def method_3():
     return "This is method 3"
+
+def message(num1, num2):
+    return "The sum of {} and {} is {}".format(num1, num2, sum(num1, num2))
