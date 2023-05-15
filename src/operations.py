ac174aaf6a18cdbd72073fc19daa37fab6de7816
yessica-rd <yessica@leanmind.es>
2023-05-15 13:50:05 +0100

    modify operations.py

--- src/operations.py
+++ src/operations.py
@@ -1,6 +1,6 @@
 # from src.operations.divide import divide
 # from src.operations.multiply import multiply
-from operations.sumatory import sumatory
+# from operations.sumatory import sumatory
 
 
 def sum_operation(a, b):
@@ -14,3 +14,6 @@ def sum_operation(a, b):
 
 def hello():
     return "Hello world!"
+
+if __name__ == "__main__":
+    from operations.sumatory import sumatory
