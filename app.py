784417e410a4543a17e26a8a98469175518866b0
yessica-rd <yessica@leanmind.es>
2023-05-15 12:43:54 +0100

    Uncomment code from app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-# def sum(num1, num2):
-#     return message(num1, num2)
+def sum(num1, num2):
+    return message(num1, num2)
 
-# def method_2():
-#     return "This is method 2"
+def method_2():
+    return "This is method 2"
 
-# def method_3():
-#     return "This is method 3"
+def method_3():
+    return "This is method 3"
 
-# def message(num1, num2):
-#     return f'{num1} + {num2} = {num1 + num2}'
+def message(num1, num2):
+    return f'{num1} + {num2} = {num1 + num2}'
