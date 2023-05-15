8eba5f20187f05788640b39f5a18997dfd140f6b
yessica-rd <yessica@leanmind.es>
2023-05-15 12:57:01 +0100

    Create src_message.py

--- src/src_app.py
+++ src/src_app.py
@@ -1,5 +1,8 @@
+from src.src_message import print_src_message
+
+
 def sum(num1, num2):
-    return message(num1, num2)
+    return print_src_message(num1, num2)
 
 def method_2():
     return "This is method 2"
@@ -7,6 +10,3 @@ def method_2():
 def method_3():
     return "This is method 3"
 
-def message(num1, num2):
-    result = int(num1) + int(num2)
-    return f'{num1} + {num2} = {str(result)}'
---
+++ src/src_message.py
@@ -0,0 +1,3 @@
+def print_src_message(num1, num2):
+    result = int(num1) + int(num2)
+    return f'{num1} + {num2} = {str(result)}'
