5c7b7783796bd41a5e19449400a58ce426411aa9
yessica-rd <yessica@leanmind.es>
2023-05-16 08:48:47 +0100

    modify operations.py

--- src/operations.py
+++ src/operations.py
@@ -1,22 +1,11 @@
-# from src.operations.divide import divide
-# from src.operations.multiply import multiply
-# from operations.sumatory import sumatory
-
-
 def sum_operation(a, b):
     print('This is the sum_operation function')
+    from operations.sumatory import sumatory
     return sumatory(a, b)
-
-# def multiply_operation(a, b):
-#     return multiply(a, b)
-
-# def divide_operation(a, b):
-#     return divide(a, b)
-
 
 def hello():
     return "Hello world!"
 
 
-if __name__ == "__main__":
-    from operations.sumatory import sumatory
+# if __name__ == "__main__":
+#     from operations.sumatory import sumatory
